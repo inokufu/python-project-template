9d6a6dc2a62371105beb16527001dd08e0bac1fb
pierrocknroll <pierrock+github@protonmail.com>
2025-03-11 15:45:49 +0100

    :wrench: Add empty files

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+"""This file contains pytest fixtures available to all tests.
+
+Fixtures are functions that pytest runs before tests to set up preconditions.
+pytest automatically discovers this file and makes fixtures available
+to all test modules without needing to import them.
+"""
